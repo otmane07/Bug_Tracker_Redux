532402f7e5dfec067192c34b8ac516635459d4c5
Khouna Otmane <okhouna@sqli.com>
2023-04-04 15:00:45 +0000

    Work with Redux devtools extentension

--- src/store.js
+++ src/store.js
@@ -1,5 +1,8 @@
 import {createStore} from "redux";
 import bugReducer from "./reducer"
+import {devToolsEnhancer} from "redux-devtools-extension";
 
-const store = createStore(bugReducer)
+const store = createStore(bugReducer , devToolsEnhancer({trace : true}))
+// devToolsEnhancer({trace : true} work similarly to window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// but this package "redux-devtools-extension" make the configuration of the extension possible enable trace
 export default store ;
